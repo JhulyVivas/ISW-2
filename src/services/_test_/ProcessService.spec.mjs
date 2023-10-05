fbdd2fdb38ff58b6317907f1e8a5c1566106bf2c
JhulyVivas <131303835+JhulyVivas@users.noreply.github.com>
2023-10-05 17:25:39 -0500

    clase 05-10-2023

--- src/services/_test_/ProcessService.spec.mjs
+++ src/services/_test_/ProcessService.spec.mjs
@@ -1,4 +1,6 @@
-import { describe, test, expect, jest} from '@jest/globals';
+import {
+  describe, test, expect, jest,
+} from '@jest/globals';
 import ProcessRepository from '../../repositories/ProcessRepository.mjs';
 import MinioService from '../MinioService.mjs';
 import ProcessService from '../ProcessService.mjs';
@@ -17,34 +19,35 @@ describe('Test suma', () => {
 
 describe('ProcessService test', () => {
   const processRepository = new ProcessRepository();
-  const minioService = {
-    saveImage: jest.fn()
-        . mockImplementationOnce(() => Promise.resolve('image1.png')),
-  };
+
+  const minioService = new MinioService();
 
   const processService = new ProcessService({ minioService, processRepository });
 
   test('Test applyFilters function with invalid payoad', () => {
     expect(processService.applyFilters()).rejects.toThrow();
     expect(processService.applyFilters({})).rejects.toThrow();
-    expect(processService.applyFilters({ filters: []})).rejects.toThrow(); 
+    expect(processService.applyFilters({ filters: [] })).rejects.toThrow();
   });
-  test('Test applyFilters function with valid payload', async() => {
-    const payload ={
-        filters: ['negative'],
-        images: [{originalname: 'image1.png', buffer: Buffer.from('')}],
+  test('Test applyFilters function with valid payload', async () => {
+    const payload = {
+      filters: ['negative'],
+      images: [{ originalname: 'image1.png', buffer: Buffer.from('') }],
     };
     const expectedProcess = {
-        id: '1234',
-        filters: payload.filters,
-        images: payload.images,
+      id: '1234',
+      filters: payload.filters,
+      images: payload.images,
     };
 
     processRepository.save = jest.fn()
-        .mockImplementationOnce(()=>({
-            id: '1234',
-            filters: payload.filters,
-            images: payload.images,
-        
+      .mockImplementationOnce(() => expectedProcess);
+
+    minioService.saveImage = jest.fn()
+      .mockImplementationOnce(() => Promise.resolve('image1.png'));
+
+    const process = await processService.applyFilters(payload);
+
+    expect(process).toMatchObject(expectedProcess);
   });
 });
